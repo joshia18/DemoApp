79004749a7bfbaa958a317b83e8aaa38103c9b2f
joshia18 <arunjoshibond007@gmail.com>
2022-11-26 22:11:40 +0530

    second commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	public Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(1==1);
 	}
 
 }
