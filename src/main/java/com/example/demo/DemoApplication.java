7032d60c0c33b7540e05375fdceb0913ad2e1030
ajoshi <ajoshi@xactlycorp.com>
2025-01-09 19:20:55 +0530

    dummy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
+		logger.info("adding log");
 		logger.info("Starting SpringBoot Application....");
 		SpringApplication.run(DemoApplication.class, args);
 	}
